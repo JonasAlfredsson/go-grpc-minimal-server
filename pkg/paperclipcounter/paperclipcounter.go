ee2691555ae76f3e0c5765d13e49591dda976a31
Jonas Alfredsson <jonasal@axis.com>
2023-07-20 15:58:37 +0200

    Add logger statements in the gRPC functions
    Change-Id: I203e8da909e8e133622aefdaef51b0a14759281f

--- pkg/paperclipcounter/paperclipcounter.go
+++ pkg/paperclipcounter/paperclipcounter.go
@@ -2,25 +2,30 @@ package paperclipcounter
 
 import (
 	"context"
+	"fmt"
 
 	pb "github.com/JonasAlfredsson/go-grpc-minimal-server/proto"
+	"golang.org/x/exp/slog"
 )
 
 // PaperclipCounter implements the paper clip counter service.
 type PaperclipCounter struct {
 	pb.PaperclipCounterServer
+	logger     *slog.Logger
 	Paperclips int32
 }
 
 // NewServer creates an instance of the PaperclipCounter service.
-func NewServer() *PaperclipCounter {
+func NewServer(logger *slog.Logger) *PaperclipCounter {
 	return &PaperclipCounter{
+		logger:     logger,
 		Paperclips: 1,
 	}
 }
 
 // GetPaperclips returns the current paper clip count.
 func (s *PaperclipCounter) GetPaperclips(_ context.Context, _ *pb.Empty) (*pb.Paperclips, error) {
+	s.logger.Info(fmt.Sprintf("Responding with %d paperclips", s.Paperclips))
 	return &pb.Paperclips{
 		Paperclips: s.Paperclips, // Response will be empty if this is 0.
 	}, nil
@@ -28,6 +33,7 @@ func (s *PaperclipCounter) GetPaperclips(_ context.Context, _ *pb.Empty) (*pb.Pa
 
 // IncrementPaperclips increments the paperclip count.
 func (s *PaperclipCounter) IncrementPaperclips(_ context.Context, size *pb.Size) (*pb.Empty, error) {
+	s.logger.Info(fmt.Sprintf("Adding %d paperclips to the current count of %d", size.Paperclips, s.Paperclips))
 	s.Paperclips += size.Paperclips
 
 	return &pb.Empty{}, nil
